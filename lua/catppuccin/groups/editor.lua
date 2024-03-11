b0077d94b006ed0bf5c897b252a9cb0d9db52d94
Marko Samaržija <marko.samarzija@invariant.hr>
2024-03-11 19:47:03 +0100

    borders rose

--- lua/catppuccin/groups/editor.lua
+++ lua/catppuccin/groups/editor.lua
@@ -14,7 +14,7 @@ function M.get()
 		Directory = { fg = C.blue }, -- directory names (and other special names in listings)
 		EndOfBuffer = { fg = O.show_end_of_buffer and C.surface1 or C.base }, -- filler lines (~) after the end of the buffer.  By default, this is highlighted like |hl-NonText|.
 		ErrorMsg = { fg = C.red, style = { "bold", "italic" } }, -- error messages on the command line
-		VertSplit = { fg = O.transparent_background and C.surface0 or C.crust }, -- the column separating vertically split windows
+		VertSplit = { fg = O.transparent_background and C.text or C.crust }, -- the column separating vertically split windows
 		Folded = { fg = C.blue, bg = O.transparent_background and C.none or C.surface1 }, -- line used for closed folds
 		FoldColumn = { fg = C.overlay0 }, -- 'foldcolumn'
 		SignColumn = { fg = C.surface1 }, -- column where |signs| are displayed
