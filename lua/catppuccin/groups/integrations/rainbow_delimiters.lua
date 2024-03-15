3cee4f7d30d31580c288a3fbedb5737af1ca2cd1
Marko Samaržija <marko.samarzija@invariant.hr>
2024-03-15 04:38:17 +0100

    revert colors

--- lua/catppuccin/groups/integrations/rainbow_delimiters.lua
+++ lua/catppuccin/groups/integrations/rainbow_delimiters.lua
@@ -2,10 +2,10 @@ local M = {}
 
 function M.get()
 	return {
-		RainbowDelimiterRed ={ fg =  "#eb87b6" },
+		RainbowDelimiterRed ={ fg =  "#d179ee" },
 		RainbowDelimiterYellow =  { fg = "#7ac3ff"  },
 		RainbowDelimiterBlue = {fg =  "#fc50b4" },
-		RainbowDelimiterOrange =  { fg =   "#d179ee"},
+		RainbowDelimiterOrange =  { fg =   "#eb87b6"},
 		RainbowDelimiterGreen = { fg =  "#4fc8b7" },
 		RainbowDelimiterViolet = { fg =  "#f9e2af"},
 		RainbowDelimiterCyan = {fg ="#FDFD97" },
