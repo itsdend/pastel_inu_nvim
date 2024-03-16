9844432a7c1c634e56291e155d6de2a58f3b9b86
Marko Samaržija <marko.samarzija@invariant.hr>
2024-03-16 20:46:25 +0100

    now maybe fix

--- after/queries/erlang/highlights.scm
+++ after/queries/erlang/highlights.scm
@@ -5,5 +5,5 @@
 (callback (atom) @function) @keyword.import.erlang
 (function_clause
   name: (atom) @type)
-(spec fun:(atom) @module, ty:(atom) @constant) @module
+(spec fun: (atom) @module) @module
 
