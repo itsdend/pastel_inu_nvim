be53575ba84a688c9dc936faec6db5a8115d85a6
Marko Samaržija <marko.samarzija@invariant.hr>
2024-03-16 20:03:27 +0100

    daj vise radi

--- after/queries/erlang/highlights.scm
+++ after/queries/erlang/highlights.scm
@@ -1,8 +1,8 @@
 ;; extends
 (remote_module (atom) @module) @module
- (behaviour_attribute (@atom) @module) 
-; (tuple (atom) @constant) @type.erlang
-; (callback (atom) @function) @keword
+ (behaviour_attribute (atom) @module) 
+(tuple (atom) @constant) 
+(callback (atom) @function) 
 (function_clause
   name: (atom) @type)
 (spec) @module
