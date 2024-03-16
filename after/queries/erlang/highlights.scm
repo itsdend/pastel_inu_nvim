0c482698dbad5964572c6fe620ef0015248da0b4
Marko Samaržija <marko.samarzija@invariant.hr>
2024-03-16 21:36:09 +0100

    radi kao

--- after/queries/erlang/highlights.scm
+++ after/queries/erlang/highlights.scm
@@ -5,6 +5,7 @@
 (callback (atom) @function) @keyword.import.erlang
 (function_clause
   name: (atom) @type)
-; (spec fun: (atom) @module  ) @module
+(spec fun: (atom) @module  ) @module
 (spec sigs: (type_sig (atom) @constant) )
+(spec sigs: (type_sig (pipe (atom) @constant)) )
 
